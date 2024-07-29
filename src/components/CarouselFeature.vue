41e585e651c8e6841af578d632f8272830959696
VrhovacDavid <david.vrhovac@tilltech.ch>
2024-07-29 13:02:43 +0200

    spacing

--- src/components/CarouselFeature.vue
+++ src/components/CarouselFeature.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="mx-auto mb-2 text-start px-4">
+        <div class="mx-auto text-start px-4">
 
 <h2 class="mb-2 text-mobile-h2 leading-mobile-h2 font-black tracking-tight text-headings  ">Es war noch nie so einfach!</h2>
 <p class="   text-highlights text-mobile-bodyLG">Geniesse die Freiheit, dich auf dein Geschäft zu konzentrieren, während wir uns um deine kreativen Bedürfnisse kümmern.</p>
